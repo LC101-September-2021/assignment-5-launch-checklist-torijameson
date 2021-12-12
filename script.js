bbeb0b96eeb733185492e0ce8ded00b969ef64f1
torijameson <vtjameson@gmail.com>
2021-12-11 20:09:46 -0600

    12/11 still no planet except i didnt touch the planet code. I want to kick assignment 5 into one of these suns

--- script.js
+++ script.js
@@ -1,43 +1,93 @@
 //const { formSubmission } = require("./scriptHelper");
+​
+// Write your JavaScript code here!
+window.addEventListener("load", function () {
+  let listedPlanets;
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+​
+      let selectedPlanet = pickPlanet(listedPlanets);
+      console.log(selectedPlanet);
+      addDestinationInfo(
+        document,
+        selectedPlanet.name,
+        selectedPlanet.diameter,
+        selectedPlanet.star,
+        selectedPlanet.distance,
+        selectedPlanet.moons,
+        selectedPlanet.image
+      );
+    });
+  let list = document.getElementById("faultyItems");
+  list.style.visibility = "hidden";
+  let form = document.querySelector("form");
+​
+  window.addEventListener("submit", function (event) {
+    event.preventDefault();
+​
+    let pilotInput = document.querySelector("input[name=pilotName]");
+    let pilot = pilotInput.value;
+​
+    let copilotInput = document.querySelector("input[name=copilotName]");
+    let copilot = copilotInput.value;
+​
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = fuelLevelInput.value;
+​
+    let cargoLevelInput = document.querySelector("input[name=cargoMass]");
+    let cargoLevel = cargoLevelInput.value;
+​
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+  });
+});
+
+
+//const { formSubmission } = require("./scriptHelper");
 
 
 // Write your JavaScript code here!
-window.addEventListener("load", function() {
+// window.addEventListener("load", function() {
 
-   let listedPlanets;
-   let listedPlanetsResponse=myFetch(); 
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-       }).then(function () {
-       console.log(listedPlanets);
+//    let listedPlanets;
+//    let listedPlanetsResponse=myFetch(); 
+//    listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//        }).then(function () {
+//        console.log(listedPlanets);
     
     
-    let selectedPlanet = pickPlanet(listedPlanets); console.log(selectedPlanet.image)
-    addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image)
+//     let selectedPlanet = pickPlanet(listedPlanets); console.log(selectedPlanet.image)
+//     addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image)
     
-    })
-let list = document.getElementById("faultyItems")
-    list.style.visibility="hidden";
-    let form = document.querySelector("form")
+//     })
+// let list = document.getElementById("faultyItems")
+//     list.style.visibility="hidden";
+//     let form = document.querySelector("form")
 
 
-window.addEventListener("submit", function(event) {
-    event.preventDefault();
+// window.addEventListener("submit", function(event) {
+//     event.preventDefault();
 
-    let pilotInput=document.querySelector("input[name=pilotName]")
-    let pilot=pilotInput.value
+//     let pilotInput=document.querySelector("input[name=pilotName]")
+//     let pilot=pilotInput.value
 
-    let copilotInput=document.querySelector("input[name=copilotName]")
-    let copilot=copilotInput.value
+//     let copilotInput=document.querySelector("input[name=copilotName]")
+//     let copilot=copilotInput.value
 
-    let fuelLevelInput=document.querySelector("input[name=fuelLevel]")
-    let fuelLevel=fuelLevelInput.value
+//     let fuelLevelInput=document.querySelector("input[name=fuelLevel]")
+//     let fuelLevel=fuelLevelInput.value
 
-    let cargoLevelInput=document.querySelector("input[name=cargoLevel")
-    let cargoLevel=cargoLevelInput.value
+//     let cargoLevelInput=document.querySelector("input[name=cargoLevel")
+//     let cargoLevel=cargoLevelInput.value
 
-    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)
+//     formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel)
 
-});   
-});
+// });   
+// });
